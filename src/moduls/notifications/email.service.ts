ea56bff03bb02ba1b72286807b9135999030a3bc
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-11-25 12:50:29 +0400

    some stuff

--- src/moduls/notifications/email.service.ts
+++ src/moduls/notifications/email.service.ts
@@ -6,7 +6,7 @@ export class EmailService {
   constructor(private mailerService: MailerService) {}
 
   async sendConfirmationEmail(email: string, code: string): Promise<void> {
-    //can add html templates, implement advertising and other logic for mailing.ghjgjghg
+    //can add html templates, implement advertising and other logic for mailing
     await this.mailerService.sendMail({
       from: 'Miha <miha25-2010@mail.ru>',
       to: email,
